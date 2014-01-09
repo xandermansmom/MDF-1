b6d878bc43fcd9d63ebb80e624d2d3d66fec3a27
Michele Laramore <xandermansmom@fullsail.edu>
2014-01-09 12:07:00 -0600

    Initial Commit

---
+++ MDF 1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MDF 1
+//
+//  Created by Michele Laramore on 1/9/14.
+//  Copyright (c) 2014 Michele Laramore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MDF 1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDF 1
+//
+//  Created by Michele Laramore on 1/9/14.
+//  Copyright (c) 2014 Michele Laramore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
